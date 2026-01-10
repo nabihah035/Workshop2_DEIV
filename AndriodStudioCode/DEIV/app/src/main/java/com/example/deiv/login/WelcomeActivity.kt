e400e1b5b7baf244ad412e1054d870c0c9ce3b70
nabihah035 <nabihahzulkefli1@gmail.com>
2026-01-11 04:45:20 +0800

    enhance ui for user profile, add splash screen,

--- AndriodStudioCode/DEIV/app/src/main/java/com/example/deiv/login/WelcomeActivity.kt
+++ AndriodStudioCode/DEIV/app/src/main/java/com/example/deiv/login/WelcomeActivity.kt
@@ -4,8 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-
-// --- CRITICAL FIX: Import your app's R file ---
 import com.example.deiv.R
 
 class WelcomeActivity : AppCompatActivity() {
@@ -13,17 +11,14 @@ class WelcomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
 
-        // Clean Kotlin syntax (removed '?' to prevent null errors)
         val btnLogin = findViewById<Button>(R.id.btnGoToLogin)
         val btnRegister = findViewById<Button>(R.id.btnGoToRegister)
 
-        // Click Login -> Go to Login Page
         btnLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
-        // Click Register -> Go to Register Page
         btnRegister.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
